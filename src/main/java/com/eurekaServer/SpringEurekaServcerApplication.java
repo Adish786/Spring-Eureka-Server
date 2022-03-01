788ca0878648990b42d731543c6aac08a70bcea9
Adish Ansari <adishansari786@gmail.com>
2022-03-01 10:05:41 +0530

    Eureka Server

---
+++ src/main/java/com/eurekaServer/SpringEurekaServcerApplication.java
@@ -0,0 +1,15 @@
+package com.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringEurekaServcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServcerApplication.class, args);
+	}
+
+}
